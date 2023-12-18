d70ae1cd651d8dcf432998d24f78d893b20a7a1b
Muhammad Yusuf Alpian <llawliet1945@gmail.com>
2023-12-18 22:07:29 +0700

    fix activation user

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -18,7 +18,7 @@ const logger = winston.createLogger({
 });
 
 class AuthController {
-    signin (req: Request, res: Response) {
+    signin = (req: Request, res: Response) => {
         const secret: any = process.env.JWT_SECRET;
         User.findOne({ where: { userUsername: req.body.userUsername, userStatus: 0 } }).then( dataUser => {
             console.log(dataUser);
@@ -35,7 +35,7 @@ class AuthController {
             return ApiResponse.error(`login failed`, error, res);
         });
     }
-    async signup(req: Request, res: Response) {
+    async signup (req: Request, res: Response) {
         if(req.body.userPass != req.body.userConfirmPass) return ApiResponse.badRequest(`Password and confirmation password not match`, null, res);
         const checkUser = await User.findOne({ where: { userUsername: req.body.userUsername, userStatus: 0 } });
         if(checkUser) return ApiResponse.badRequest(`username is already exists`, null, res);
@@ -45,7 +45,7 @@ class AuthController {
             userPass: pass,
             userEmail: req.body.userEmail,
             userRoleId: req.body.userRoleId,
-            userStatus: 1
+            userStatus: 2
         });
         const mailTemplate = await MailTempalte.findOne({where : { mailTemplateCode: `otp-activation-account`}});
         if (!mailTemplate) return ApiResponse.notFound("Template email not found", null, res);
@@ -60,29 +60,23 @@ class AuthController {
         return ApiResponse.created(`create user success, please check your email for activation your account`, null, res);
     }
 
-    activationUser(req:Request, res: Response) {
-        UserOtp.findOne( { where : { userOtpCode : req.params.otp } }).then( otp => {
+    activationUser = (req: Request, res: Response) => {
+        UserOtp.findOne({ where: { userOtpCode: req.params.otp } }).then(otp => {
             if (!otp) return ApiResponse.notFound("Otp not found!", null, res);
-            if (otp.userOtpExpiredDate != undefined && otp.userOtpExpiredDate < new Date()) {
-                console.log('expired');
-                otp.update({userOtpStatus: 2}).then( disactive => {
-                    User.update({ userStatus: 2 }, { where: { userId: disactive.createdBy } }).then( updated => {
-                        return ApiResponse.badRequest("Otp was expired", null, res);
-                    }).catch( error => { return ApiResponse.error(`delete data failed`, error, res); });
-                })
-            } else {
-                console.log('expired');
-                otp.update({userOtpStatus: 1}).then( disactive => {
-                    User.update({ userStatus: 0 }, { where: { userId: otp.createdBy } }).then( updated => {
-                        return ApiResponse.success("success activation user", null, res);
-                    }).catch( error => { return ApiResponse.error(`delete data failed`, error, res); });
-                })
-            }
-        });
+            User.findOne({ where: { userId: otp.createdBy, userStatus: 2 } }).then(user => {
+                if (!user) return ApiResponse.notFound("User not found", null, res);
+                if (otp.userOtpExpiredDate !== undefined && otp.userOtpExpiredDate < new Date()) {
+                    return otp.update({ userOtpStatus: 1, updatedBy: user.userId, updatedDate: new Date()}).then(disactive => 
+                        User.update({ userStatus: 1, updatedBy: user.userId, updatedDate: new Date()}, { where: { userId: user.userId } })).then(() => ApiResponse.badRequest("Otp was expired", null, res)).catch(error => ApiResponse.error(`update data failed`, error, res));
+                }
+                return otp.update({ userOtpStatus: 2, updatedBy: user.userId, updatedDate: new Date()}).then(disactive => 
+                    User.update({ userStatus: 0, updatedBy: user.userId, updatedDate: new Date()}, { where: { userId: user.userId } })).then(() => ApiResponse.success("Success activation user", null, res))
+                .catch(error => ApiResponse.error(`update data failed`, error, res));
+            }).catch(error => ApiResponse.error("Error finding user", error, res));
+        }).catch(error => ApiResponse.error("Error finding OTP", error, res));
     }
 }
 
-// @ts-ignore
 const generateOtp = async (userId: number) : Promise<string>  => {
     let otp = Math.floor(100000 + Math.random() * 900000);
     let now : Date = new Date();
